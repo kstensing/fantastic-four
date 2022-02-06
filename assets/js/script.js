563aab2c4a793eb1a8f5eaf485b5c62e0798fb06
Caleb Luster <cwl117@outlook.com>
2022-02-06 10:32:05 -0600

    added modal prompt when user does not enter a city

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,29 @@ var formCityHandler = function (event) {
     if (city) {
         getCity(city);
     } else {
-        alert("PLease enter a valid city *change to modal*");
+        //   Get the modal
+            var modal = document.getElementById("myModal");
+        
+        //   Get the <span> element that closes the modal
+            var span = document.getElementsByClassName("close")[0];
+        
+        //   When the user clicks on the button, open the modal
+            function noCity() {
+            modal.style.display = "block";
+            }
+        
+        //   When the user clicks on <span> (x), close the modal
+            span.onclick = function() {
+            modal.style.display = "none";
+            }
+        
+        //   When the user clicks anywhere outside of the modal, close it
+            window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+            }
+            noCity();
     };
 };
 
@@ -56,3 +78,7 @@ var displayCity = function (cityInfo) {
 
 // submit city button event listener to trigger form submit handler
 cityFormEl.addEventListener("submit", formCityHandler);
+
+
+
+
